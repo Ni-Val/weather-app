32a973f98b8eff9b58cbe2c5a241a4d7b7e8f637
Ni-Val <nivale2003@gmail.com>
2025-09-13 17:27:54 -0400

    added search functionality (API)
    connected API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "ft0bf45d0b07feb3a4e2d5oa86cd5037";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 //search engine function
 function handleSearchSubmit(event) {
   event.preventDefault();
@@ -5,6 +19,9 @@ function handleSearchSubmit(event) {
   console.log(searchInput.value);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Seoul");
