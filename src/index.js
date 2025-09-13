5f483bb0c153560757fa1d95b193949ae56010d3
Ni-Val <nivale2003@gmail.com>
2025-09-13 15:58:49 -0400

    added basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//search engine function
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input-form");
+  console.log(searchInput.value);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
